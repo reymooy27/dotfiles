8d75234cbb1529ea443b13391498bf7a5b183507
reymooy27 <gdrrey@gmail.com>
2024-02-05 22:52:15 +0800

    add eslint lsp

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -6,6 +6,7 @@ return {
       ---@type lspconfig.options
       servers = {
         -- pyright will be automatically installed with mason and loaded with lspconfig
+        eslint = {},
         pyright = {},
         tsserver = {},
         html = {},
@@ -27,6 +28,17 @@ return {
             "typescript.tsx",
           },
         },
+      },
+      setup = {
+        eslint = function()
+          require("lazyvim.util").lsp.on_attach(function(client)
+            if client.name == "eslint" then
+              client.server_capabilities.documentFormattingProvider = true
+            elseif client.name == "tsserver" then
+              client.server_capabilities.documentFormattingProvider = false
+            end
+          end)
+        end,
       },
     },
   }, -- configure html server   -- configure html server
